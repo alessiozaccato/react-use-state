1c55946cc7cd203f157f5f5b6676fc54deea2412
alessiozaccato <alessiozaccato@gmail.com>
2025-02-21 20:19:40 +0100

    ADD: logic on click

--- src/components/ButtonsList.jsx
+++ src/components/ButtonsList.jsx
@@ -4,10 +4,6 @@ const ButtonsList = (props) => {
 
     const { languages } = props
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleLangs = () => setIsOpen(!isOpen);
-
     // user experience to check the content of the array
     if (languages.length === 0) {
         return <h2>Non ci sono Linguaggi</h2>
@@ -21,18 +17,18 @@ const ButtonsList = (props) => {
 
             const { id, title, description } = language;
 
+            const [idDesc, setidDesc] = useState(false);
+
+            const handleLangs = () => setidDesc(!idDesc);
+
             return (
 
-
-                < div className="lang" key={id} >
-                    <button onClick={handleLangs} > {title}</button>
+                <div className="lang" key={id} >
+                    <button className='button' onClick={handleLangs}> {title}</button>
                     <div className='btnDescription'>
-                        {isOpen && < div> {description} </div>}
+                        {idDesc && < div> {description} </div>}
                     </div >
                 </div >
-
-
-
 
 
             )
